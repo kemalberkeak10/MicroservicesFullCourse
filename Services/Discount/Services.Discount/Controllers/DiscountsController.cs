85565b6d6711d48cad3e741198a63e50dfcb9a27
Berke AK <berke.ak@argeset.com>
2023-08-05 16:59:13 +0300

    DiscountAPI CRUD and Dapper works

---
+++ Services/Discount/Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Discount.Services;
+using Shared.ControllerBases;
+using Shared.Services;
+using System.Threading.Tasks;
+
+namespace Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+        private readonly IDiscountService _discountService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountsController(ISharedIdentityService sharedIdentityService, IDiscountService discountService)
+        {
+            _sharedIdentityService = sharedIdentityService;
+            _discountService = discountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _discountService.GetAll();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id){
+            var discount = await _discountService.GetById(id);
+            return CreateActionResultInstance(discount);
+        }
+
+        [HttpGet("/api/[controller]/[action]/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            var userId = _sharedIdentityService.GetUserId;
+
+            var discount = await _discountService.GetByCodeAndUserId(code,userId);
+            return CreateActionResultInstance(discount);
+        }
+        [HttpPost, Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> Save(Models.Discount discount)
+        {
+            var response = await _discountService.Save(discount);
+            return CreateActionResultInstance(response);
+        }
+
+
+        [HttpPost,Route("/api/[controller]/[action]")]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+             var response = await _discountService.Update(discount);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _discountService.Delete(id);
+            return CreateActionResultInstance(response);
+        }   
+    }
+}
--- Services/Discount/Services.Discount/Startup.cs
+++ Services/Discount/Services.Discount/Startup.cs
@@ -1,13 +1,19 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Services.Discount.Services;
+using Shared.Services;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,9 +31,28 @@ namespace Services.Discount
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();//HttpContext eriþimi için
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddScoped<IDiscountService, DiscountService>();
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            //user sadece okuma izni olsa
+            //new AuthorizationPolicyBuilder().RequireClaim("scope", "discount_readonly");//bu claim varsa okuma izni var demek
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");//sub claimini maplemez
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServerURL"]; //Token daðýtmaktan görevli
+                opt.Audience = "resource_discount";// Tokeni kullanacak olan
+                opt.RequireHttpsMetadata = false; // https kullanmýyoruz
+            });
+
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+            });
+        services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Services.Discount", Version = "v1" });
             });
@@ -44,7 +69,7 @@ namespace Services.Discount
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
