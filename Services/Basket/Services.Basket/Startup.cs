62cc567d0957f58dc8be4a61336a4197883863b9
Berke AK <berke.ak@argeset.com>
2023-08-05 13:13:02 +0300

    BasketAPI CRUD

--- Services/Basket/Services.Basket/Startup.cs
+++ Services/Basket/Services.Basket/Startup.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,6 +15,7 @@ using Services.Basket.Settings;
 using Shared.Services;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,8 +33,18 @@ namespace Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");//sub claimini maplemez
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServerURL"]; //Token daðýtmaktan görevli
+                opt.Audience = "resource_basket";// Tokeni kullanacak olan
+                opt.RequireHttpsMetadata = false; // https kullanmýyoruz
+            });
+
             services.AddHttpContextAccessor();// httpcontext eriþimi için
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddScoped<IBasketService, BasketService>();
             services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
 
             services.AddSingleton<RedisService>(services =>
@@ -40,7 +54,8 @@ namespace Services.Basket
                 redisService.Connect();
                 return redisService;
             });
-            services.AddControllers();
+            services.AddControllers(opt=>
+            opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy)));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Services.Basket", Version = "v1" });
@@ -58,7 +73,7 @@ namespace Services.Basket
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
