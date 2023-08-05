693130322db5f0bc0701ce08cdb829668d696891
Berke AK <berke.ak@argeset.com>
2023-08-05 22:03:36 +0300

    OrderAPI Controller works

--- Services/Order/Services.Order.API/Startup.cs
+++ Services/Order/Services.Order.API/Startup.cs
@@ -1,6 +1,10 @@
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,8 +12,10 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Services.Order.Infrastructure;
+using Shared.Services;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -27,7 +33,16 @@ namespace Services.Order.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");//sub claimini maplemez
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.Authority = Configuration["IdentityServerURL"]; //Token daðýtmaktan görevli
+                opt.Audience = "resource_order";// Tokeni kullanacak olan
+                opt.RequireHttpsMetadata = false; // https kullanmýyoruz
+            });
 
+            services.AddHttpContextAccessor();
             services.AddDbContext<OrderDbContext>(opt =>
             {
 
@@ -36,7 +51,12 @@ namespace Services.Order.API
                     configure.MigrationsAssembly("Services.Order.Infrastructure");
                 });
             });
-            services.AddControllers();
+            services.AddMediatR(typeof(Services.Order.Application.Handlers.CreateOrderCommandHandler).Assembly);
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Services.Order.API", Version = "v1" });
@@ -54,7 +74,7 @@ namespace Services.Order.API
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
