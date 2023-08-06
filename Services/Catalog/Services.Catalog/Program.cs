f49e2f10d0375b61f17d103e74a5a9b919299791
Berke AK <berke.ak@argeset.com>
2023-08-06 12:11:26 +0300

    Adding OcelotAPI

--- Services/Catalog/Services.Catalog/Program.cs
+++ Services/Catalog/Services.Catalog/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Services.Catalog.Dtos;
+using Services.Catalog.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,23 @@ namespace Services.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          var host=CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var categoryService = serviceProvider.GetRequiredService<ICategoryService>();   
+
+                if(!categoryService.GetAllAsync().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryDto { Name = "Asp.Net Core" }).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name = "Asp.Net Mvc" }).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name = "Asp.Net Web Forms" }).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name = "Windows Form" }).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name = "Xamarin Form" }).Wait();
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
